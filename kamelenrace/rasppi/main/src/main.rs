8f10545f834a3fb6e214c3c829595bed34384a48
Mitsie01 <mitch.kampert@gmail.com>
2022-11-10 11:41:35 +0100

    new file system and testing sink function

--- kamelenrace/rasppi/main/src/main.rs
+++ kamelenrace/rasppi/main/src/main.rs
@@ -1,24 +1,47 @@
 use std::fs::File;
 use std::io::BufReader;
-use rodio::{Decoder, OutputStream, source::Source};
+use rodio::{Decoder, OutputStream, Sink, source::Source};
+use std::fs;
 
 fn main () {
     // Get a output stream handle to the default physical sound device
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    // Load a sound from a file, using a path relative to Cargo.toml
-    let fx1 = BufReader::new(File::open("../audio/fx/lessgo.mp3").unwrap());
-    let fx2 = BufReader::new(File::open("../audio/fx/mario-kart.mp3").unwrap());
+    let sink = Sink::try_new(&stream_handle).unwrap();
 
-    let idle1 = BufReader::new(File::open("../audio/idle/cleopatra.mp3").unwrap());
-    let idle2 = BufReader::new(File::open("../audio/idle/prince-of-egypt.mp3").unwrap());
-    let idle3 = BufReader::new(File::open("../audio/idle/princess-of-persia.mp3").unwrap());
-    let idle4 = BufReader::new(File::open("../audio/idle/sands-of-arabia.mp3").unwrap());
-    // Decode that sound file into a source
-    let source = Decoder::new(fx2).unwrap();
-    // Play the sound directly on the device
-    stream_handle.play_raw(source.convert_samples());
+    // Get all file paths 
+    let mut paths = fs::read_dir("../audio/fx/").unwrap();
 
-    // The sound plays in a separate audio thread,
-    // so we need to keep the main thread alive while it's playing.
-    std::thread::sleep(std::time::Duration::from_secs(2));
+    println!("Paths to fx sounds:");
+    for path in paths {
+        println!("{}", path.unwrap().path().display());
+    }
+    
+    paths = fs::read_dir("../audio/idle/").unwrap();
+
+    println!("Paths to idle music:");
+    for path in paths {
+        println!("{}", path.unwrap().path().display());
+    }
+
+
+    // Arrays containing file paths
+    let fx_paths = ["../audio/fx/lessgo.mp3", "../audio/fx/mario-kart.mp3"];
+    let idle_paths = ["../audio/idle/cleopatra.mp3", "../audio/idle/prince-of-egypt.mp3", "../audio/idle/princess-of-persia.mp3", "../audio/idle/sands-of-arabia.mp3"];
+
+
+    // Load file from path list - path relative form Cargo.toml
+    let fx = BufReader::new(File::open(fx_paths[0]).unwrap());
+    let idle = BufReader::new(File::open(idle_paths[0]).unwrap());
+
+    // Decode and play sound
+    let source = Decoder::new(fx).unwrap();
+    sink.append(source);
+
+    sink.sleep_until_end();
+    
+    // stream_handle.play_raw(source.convert_samples());
+
+
+    // Keep program active to resume playing sound
+    // std::thread::sleep(std::time::Duration::from_secs(2));
 }
